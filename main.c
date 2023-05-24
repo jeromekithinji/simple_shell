11a5de2f3fcb172c3315216bab6e5db6747ed6f6
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-05-24 13:52:36 +0100

    Update main.c - Task 2

--- main.c
+++ main.c
@@ -2,15 +2,35 @@
 #include "main.h"
 
 /**
-  * main - entry point.
+  * main - entry point
   *
-  * Return: 0 on success.
+  * Return: 0 on success
   */
 
 int main(void)
 {
 	int result = add_numbers(5, 10);
+	char command[BUFFER_SIZE];
+	ssize_t bytes_read;
 
+	while (1)
+	{
+		printf("$ ");
+		fflush(stdout);
+		bytesRead = get_command(command);
+		
+		if (bytesRead == -1)
+			continue;
+		if (bytesRead == 0)
+		{
+			printf("\n");
+			break;
+		}
+		command[bytesRead - 1] = '\0';
+
+		if (execute_command(command) == -1)
+			continue;
+	}
 	printf("The result is: %d\n", result);
 
 	return (0);
