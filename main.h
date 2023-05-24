492fb34d963d21e114920f4202cba0a6b92a3bf2
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-05-24 13:34:33 +0100

    Update main.h - Task 2

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 int add_numbers(int a, int b);
+int execute_command(char *command);
+ssize_t get_command(char *command);
 
 #endif
