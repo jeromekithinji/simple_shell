baef4a560c1504da1a7df3c673a5905ca9ad49ee
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-05-25 21:12:04 +0100

    Update exec_cmnd.c - Task 4

--- exec_cmnd.c
+++ exec_cmnd.c
@@ -15,10 +15,8 @@ int get_command(char *command)
 	if (bytesRead == -1)
 		perror("read");
 
-	free(command);
-	return (bytesRead);
+	return bytesRead;
 }
-
 
 /**
  * execute_command - Execute a command given
@@ -26,6 +24,7 @@ int get_command(char *command)
  * @command: Command to execute
  * Return: 0 on success, -1 on failure
  */
+
 int execute_command(char *command)
 {
 	pid_t pid;
@@ -35,18 +34,29 @@ int execute_command(char *command)
 	if (strcmp(command, "exit") == 0)
 		exit(0);
 
+	if (strcmp(command, "env") == 0)
+	{
+		char **env = environ;
+		while (*env)
+		{
+			printf("%s\n", *env);
+			env++;
+		}
+		return 0;
+	}
+
 	pid = fork();
 
 	if (pid == -1)
 	{
 		perror("fork");
-		return (-1);
+		return -1;
 	}
-
+	
 	if (pid == 0)
 	{
 		argv = malloc(3 * sizeof(char *));
-
+		
 		if (argv == NULL)
 		{
 			perror("malloc");
@@ -57,7 +67,7 @@ int execute_command(char *command)
 		argv[1] = "-c";
 		argv[2] = command;
 		argv[3] = NULL;
-
+		
 		if (execve(argv[0], argv, NULL) == -1)
 		{
 			perror("execve");
@@ -71,5 +81,5 @@ int execute_command(char *command)
 	}
 
 	free(argv);
-	return (0);
+	return 0;
 }
